882ff3ee710890930a11cd6b4ed8ec3d258d450a
Donghwi_Park <gptnc5100@gmail.com>
2024-10-03 10:34:05 +0200

    set red, green, blue values.

--- Maui_Project - Colour Picker/MainPage.xaml.cs
+++ Maui_Project - Colour Picker/MainPage.xaml.cs
@@ -1,12 +1,30 @@
-﻿namespace Maui_Project___Colour_Picker
+﻿
+namespace Maui_Project___Colour_Picker
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = sldRed.Value;
+            var green = sldGree.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+
+        }
+
+        private void SetColor(Color color)
+        {
+            btnRandom.BackgroundColor = color;
+            Container.BackgroundColor = color;
+            lblHex.Text = color.ToHex();
         }
     }
 }
